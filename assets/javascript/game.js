3f3aab22d7a883f9ab6b7ed3d12b6935ba1871e1
Matthew Gusmano <mgusmano@Matthews-MacBook-Air.local>
2016-04-16 22:34:03 -0400

    all

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//setting vars
+
+var wins = 0;
+
+document.onkeyup = function(event) {
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+var mysteryword = {peach, bowser, mushroom, fireball, star}
+
+for (var i = 1, i = mysteryword.lenth i++) {
+	
+}
