34dbf4a95450c765b480cbf4c17501208c120c87
Matthew Gusmano <mgusmano@Matthews-MacBook-Air.local>
2016-04-20 20:02:07 -0400

    Added to java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,58 @@
+
+
+
+$(document).ready(function(){
+
 //setting vars
 
-var wins = 0;
-var mysteryword = [peach, bowser, mushroom, fireball, star, castle, luigi]
-var wordselector = mysteryword[Math.floor(Math.random() * mysteryword.lenth)];
-var space = "";
-var letters = word.split('');
-var guesses = 10;
+	var wins = 0;
+	
+	var wordSelector = mysteryword[Math.floor(Math.random() * mysteryword.lenth)];
+	var spaceDash = "";
+	var letters = wordSelector.split('');
+	var guesses = 10;
+	var placeholder = "_"
+	var guessedLetters = []
 
-for (var num = 0; num < letters.length; num++) {
-	space = space + "_";
-}
+	var game = {
+		mysteryword:["peach", "bowser", "mushroom", "fireball", "star", "castle", "luigi"],
+		lettersGuessed: [],
+		
+		totalLetters: [],
+		
+		randword: "",
+		
+		
+		}
+
+	game.randword = game.mysterword[Math.floor(Math.random() * mysteryword.lenth)];
+	console.log(game.randword);
 
 
-if (userGuess=)
 
-document.onkeyup = function(event) {
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+	
+	// var winTotal =$('<div>');
+	// winTotal = " 23232"
+	// $("#wins").append(winTotal
+
+
+
+	
+
+
+})
+
+	// for (var num = 0; num < letters.length; num++) {
+	// spaceDash = spaceDash + "_";
+	// }
+
+
+	// if (userGuess=){}
+
+	// document.onkeyup = function(event) {
+	// var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	// }
 
 
 
